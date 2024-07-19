44055690d45fce7544d0781f19ee5b7d57a1b7ff
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-07-19 16:17:08 +0300

    add

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { getAllContacts, getContactById } from './services/contacts.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 const setupServer = () => {
   const app = express();
