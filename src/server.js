7faa65e03e13bd3d79b25fa9d381b8e66127635f
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-07-22 16:11:50 +0300

    add

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ import initMongoConnection from '../src/db/initMongoConnection.js';
 
 dotenv.config();
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8009;
 
 const setupServer = async () => {
   try {
