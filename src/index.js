b23deeb9e2d489c000a39702b0395a4a881ad9d8
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-08-09 15:54:01 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,40 +1,42 @@
 import dotenv from 'dotenv';
-import express from 'express';
 import mongoose from 'mongoose';
 import User from './db/models/user.js';
 import bcrypt from 'bcrypt';
 
 dotenv.config();
 
-const app = express();
-const PORT = process.env.PORT || 4010;
-
 const createUser = async () => {
-  try {
-    const uri = process.env.MONGODB_URL;
-    if (!uri) {
-      throw new Error('MONGODB_URL is not defined');
+    try {
+      
+        const uri = `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_URL}/${process.env.MONGODB_DB}?retryWrites=true&w=majority`;
+
+      
+        if (!uri) {
+            throw new Error('MongoDB connection string is not defined');
+        }
+
+     
+        await mongoose.connect(uri);
+
+       
+        await User.deleteOne({ email: 'tour@gmail.com' });
+
+  
+        const hashedPassword = await bcrypt.hash('securepassword', 10);
+        const newUser = new User({
+            name: 'Tour Guide',
+            email: 'tour@gmail.com',
+            password: hashedPassword
+        });
+
+        await newUser.save();
+        console.log('User created successfully');
+    } catch (error) {
+        console.error('Error creating user:', error.message);
+    } finally {
+       
+        mongoose.connection.close();
     }
-
-    await mongoose.connect(uri);
-
-    const hashedPassword = await bcrypt.hash('securepassword', 10);
-    const newUser = new User({
-      name: 'Ali',
-      email: 'tour@gmail.com',
-      password: hashedPassword,
-    });
-    await newUser.save();
-    console.log('User created successfully');
-  } catch (error) {
-    console.error('Error creating user:', error.message);
-  } finally {
-    mongoose.connection.close();
-  }
 };
 
 createUser();
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
