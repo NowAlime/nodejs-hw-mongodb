65c34f6bd60d5850584534a4ea5ba1d1121542ac
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-08-10 13:42:32 +0300

    add

--- src/server.js
+++ src/server.js
@@ -8,34 +8,39 @@ import authRouter from './routers/auth.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 
-
 dotenv.config();
 
 const PORT = process.env.PORT || 4010;
 
 const setupServer = async () => {
   const app = express();
-
+  
   try {
     await initMongoConnection();
 
-    app.use(cors());
-    app.use(express.json());
+    
     app.use(pino({ transport: { target: 'pino-pretty' } }));
 
    
-    app.use('/api/auth', authRouter);
-    app.use('/contacts', contactsRouter);
+    app.use(cors());
+    app.use(express.json());
 
-    
+   
     app.use((req, res, next) => {
       console.log(`Time: ${new Date().toLocaleString()} - ${req.method} ${req.originalUrl}`);
       next();
     });
 
+   
+    app.use('/api/auth', authRouter);
+    app.use('/contacts', contactsRouter);
+
+
     app.use(notFoundHandler);
+    
     app.use(errorHandler);
 
+    
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
