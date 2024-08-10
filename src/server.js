900f2c967663f9062ee8103e3585d58194b2e73f
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-08-10 15:18:57 +0300

    add

--- src/server.js
+++ src/server.js
@@ -14,25 +14,28 @@ const PORT = process.env.PORT || 4010;
 
 const setupServer = async () => {
   const app = express();
-  
+
   try {
+    // Підключення до MongoDB
     await initMongoConnection();
 
+    // Налаштування мідлварів
     app.use(pino({ transport: { target: 'pino-pretty' } }));
     app.use(cors());
     app.use(express.json());
 
-  
     app.get('/', (req, res) => {
       res.send('Hello World!');
     });
 
+ 
     app.use('/api/auth', authRouter);
     app.use('/contacts', contactsRouter);
 
     app.use(notFoundHandler);
     app.use(errorHandler);
 
+  
     app.listen(PORT, () => {
       console.log(`Server is running on port ${PORT}`);
     });
