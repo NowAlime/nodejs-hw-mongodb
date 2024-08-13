bad316344da007fa521b71ebf2fed37210d7db1b
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-08-13 13:39:07 +0300

    add

--- src/middlewares/authenticate.js
+++ src/middlewares/authenticate.js
@@ -7,20 +7,29 @@ const authenticate = async (req, res, next) => {
   try {
     const authHeader = req.headers.authorization;
 
-    if (!authHeader) {
+    if (!authHeader || !authHeader.startsWith('Bearer ')) {
       throw createHttpError(401, 'No token provided');
     }
 
     const token = authHeader.split(' ')[1];
+
+    // Перевірка токену
     const { userId } = jwt.verify(token, process.env.JWT_SECRET);
 
+    // Перевірка сесії
     const session = await Session.findOne({ userId, accessToken: token });
 
     if (!session || new Date() > session.accessTokenValidUntil) {
       throw createHttpError(401, 'Access token expired');
     }
 
+    // Завантаження користувача
     req.user = await User.findById(userId);
+    
+    if (!req.user) {
+      throw createHttpError(401, 'User not found');
+    }
+
     next();
   } catch (error) {
     next(error);
