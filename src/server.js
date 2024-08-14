b714db23774d4a3aa5fbe50fe374169e41f529c3
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-08-14 14:18:12 +0300

    add

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import pino from 'pino-http';
-import env from './utils/env.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import router from './routers/auth.js';
 import contactsRouter from './routers/contacts.js';
 
-const PORT = Number(env('PORT', '7009'));
+const PORT = process.env.PORT || 3009; 
+
 const setupServer = () => {
   const app = express();
 
@@ -22,20 +22,16 @@ const setupServer = () => {
     }),
   );
 
-  app.use('/api', router);  
+  app.use('/api', router);
   app.use('/contacts', contactsRouter);
 
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
-  app.listen(PORT, (err) => {
-    if (err) {
-      console.error(`Error starting server: ${err.message}`);
-      process.exit(1); 
-    }
+  app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
   });
 };
 
-setupServer();
+setupServer(); 
 export default setupServer;
