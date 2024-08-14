07a88dac40fde4fafedf54783f05f14132fb8490
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-08-14 15:57:24 +0300

    add

--- src/services/auth.js
+++ src/services/auth.js
@@ -1,7 +1,7 @@
 import { randomBytes } from 'crypto';
 import bcrypt from 'bcrypt';
 import createHttpError from 'http-errors';
-import { UsersCollection } from '../db/models/User.js';
+import { UsersCollection } from '../db/models/user.js';
 import { SessionsCollection } from '../db/models/session.js';
 import { FIFTEEN_MINUTES, THIRTY_DAYS } from '../constants/index.js';
 
