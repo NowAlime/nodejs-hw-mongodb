ab678df82b6501e38bba243b91987339619e64a2
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-08-17 20:14:26 +0300

    add

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import { setupServer } from './server.js';
 import { initMongoConnection } from './db/initMongoConnection.js';
 
-const bootstrap = async () => {
+const bootUser = async () => {
   await initMongoConnection();
   setupServer();
 };
 
-bootstrap();
+bootUser();
