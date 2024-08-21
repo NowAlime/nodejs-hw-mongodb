507c4cf42442acc9f2f009e772e066afb041acf4
NowAlime <147753857+NowAlime@users.noreply.github.com>
2024-08-21 13:15:47 +0300

    add

--- src/server.js
+++ src/server.js
@@ -6,6 +6,8 @@ import env from './utils/env.js';
 import errorHandler from './middlewares/errorHandler.js';
 import notFoundHandler from './middlewares/notFoundHandler.js';
 import router from './routers/index.js';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './docs/swagger.json' assert { type: 'json' };
 
 const PORT = Number(env('PORT', '5011'));
 
@@ -25,7 +27,7 @@ export const setupServer = () => {
     res.send('Welcome to the home page!');
   });
   app.use(router);
-
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
   app.use('*', notFoundHandler);
   app.use(errorHandler);
 
